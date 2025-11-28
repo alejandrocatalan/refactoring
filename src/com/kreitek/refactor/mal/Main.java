c49577bafa2bfd37629096b128ba4b1bdcf57ba5
Alejandro Catalan <a2577@inycom.es>
2025-11-28 11:10:14 +0100

    mod variable names

--- src/com/kreitek/refactor/mal/Main.java
+++ src/com/kreitek/refactor/mal/Main.java
@@ -9,24 +9,24 @@ class  Main
         System.out.println("=====================");
 
         DNI dniCorrecto = new DNI(TIPODNI.DNI, "11111111H", null);
-        Boolean esValido = (dniCorrecto.validarDNI() == 1);
-        System.out.println( "DNI " + dniCorrecto.numDNI + " es: " + esValido);
+        Boolean esValidoDniCorrecto = (dniCorrecto.validarDNI() == 1);
+        System.out.println( "DNI " + dniCorrecto.numDNI + " es: " + esValidoDniCorrecto);
 
         DNI dniIncorrecto = new DNI(TIPODNI.DNI, "24324356A", null);
-        Boolean esValido01 = (dniIncorrecto.validarDNI() == 1);
-        System.out.println( "DNI " + dniIncorrecto.numDNI + " es: " + esValido01);
+        Boolean esValidoDniIncorrecto = (dniIncorrecto.validarDNI() == 1);
+        System.out.println( "DNI " + dniIncorrecto.numDNI + " es: " + esValidoDniIncorrecto);
 
         DNI nieCorrecto = new DNI(TIPODNI.NIE, "X0932707B", null);
-        Boolean esValidoNie = (nieCorrecto.validarDNI() == 1);
-        System.out.println( "NIE " + nieCorrecto.numDNI + " es: " + esValidoNie);
+        Boolean esValidoNieCorrecto = (nieCorrecto.validarDNI() == 1);
+        System.out.println( "NIE " + nieCorrecto.numDNI + " es: " + esValidoNieCorrecto);
 
         DNI nieIncorrecto = new DNI(TIPODNI.NIE, "Z2691139Z", null);
         Boolean esValidoNieIncorrecto = (nieIncorrecto.validarDNI() == 1);
         System.out.println( "NIE " + nieIncorrecto.numDNI + " es: " + esValidoNieIncorrecto);
 
         DNI cifCorrecto = new DNI(TIPODNI.CIF, "W9696294I", null);
-        Boolean esValidoCIF = (cifCorrecto.validarDNI() == 1);
-        System.out.println( "CIF " + cifCorrecto.numDNI + " es: " + esValidoCIF);
+        Boolean esValidoCifCorrecto = (cifCorrecto.validarDNI() == 1);
+        System.out.println( "CIF " + cifCorrecto.numDNI + " es: " + esValidoCifCorrecto);
 
         DNI cifIncorrecto = new DNI(TIPODNI.CIF, "W9696294A", null);
         Boolean esValidoCifIncorrecto = (cifIncorrecto.validarDNI() == 1);
